1b36501dadc094c8bf46e0b5e00c3f327af5b5e0
Aadesh Hemwani <aadesh.hemwani@gmail.com>
2026-02-13 00:52:03 +0530

    Initial Commit

---
+++ Expenses/ExpensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpensesApp.swift
+//  Expenses
+//
+//  Created by Adesh Hemwani on 13/02/26.
+//
+
+import SwiftUI
+
+@main
+struct ExpensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
